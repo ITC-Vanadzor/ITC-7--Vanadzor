a0fc69aeb165c5337ee99fb232aef3bf981528d5
Hasmik Naslyan <hasmik_naslyan@mail.ru>
2016-10-27 17:18:45 +0400

    version 2 has been commited :D

--- while learning js :)/WelcomeUsername/js/app.js
+++ while learning js :)/WelcomeUsername/js/app.js
@@ -1,5 +1,12 @@
 function showGreetingMessage() {
    var name = document.getElementById('uname').value;
    name = $.trim(name);
-   document.write("Welcome " + name + " !");
+   $("label").hide();
+   $("input").hide();
+   $("button").hide();
+   var para = document.createElement("p");
+   var node = document.createTextNode("Welcome " + name);
+   para.appendChild(node);
+   var element = document.getElementById("container");
+   element.appendChild(para);
 }
