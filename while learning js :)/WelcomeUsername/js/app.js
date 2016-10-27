aa009937a69af545731d19552bd2c2a5664f8ce0
Hasmik Naslyan <hasmik_naslyan@mail.ru>
2016-10-27 10:46:43 +0400

    Added an initial version of "WelcomeUsername"

---
+++ while learning js :)/WelcomeUsername/js/app.js
@@ -0,0 +1,8 @@
+function showGreetingMessage() {
+   // window.location = 'about:blank';
+   // var name = document.getElementById('uname').value;
+  //  document.write("Welcome " + name);
+    document.getElementById('form').style.display = 'none';
+
+
+}
