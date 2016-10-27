2d374ecc6be14511926cddb644c3e0888fdc2b08
Hasmik Naslyan <hasmik_naslyan@mail.ru>
2016-10-27 17:57:26 +0400

    Added form validation functionality.

--- while learning js :)/WelcomeUsername/js/app.js
+++ while learning js :)/WelcomeUsername/js/app.js
@@ -1,12 +1,23 @@
 function showGreetingMessage() {
-   var name = document.getElementById('uname').value;
-   name = $.trim(name);
+   if(validateForm()){
+   var username = document.getElementById('uname').value;
+      username = $.trim(username);
    $("label").hide();
    $("input").hide();
    $("button").hide();
-   var para = document.createElement("p");
-   var node = document.createTextNode("Welcome " + name);
-   para.appendChild(node);
+   var paragraph = document.createElement("p");
+   var node = document.createTextNode("Welcome " + username);
+      paragraph.appendChild(node);
    var element = document.getElementById("container");
-   element.appendChild(para);
+   element.appendChild(paragraph);}
+}
+
+function validateForm() {
+   var username = document.forms["form"]["uname"].value;
+   var password = document.forms["form"]["psw"].value;
+   if (username == null || username == "" || password==null || password=="") {
+      alert("The credentials of the user must be filled out");
+      return false;
+   }
+   return true;
 }
