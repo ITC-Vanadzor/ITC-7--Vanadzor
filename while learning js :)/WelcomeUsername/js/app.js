1add7331e726fe90632bb06eeae6391a7fde2137
Hasmik Naslyan <hasmik_naslyan@mail.ru>
2016-10-27 14:31:36 +0400

    Made cosmetic changes.

--- while learning js :)/WelcomeUsername/js/app.js
+++ while learning js :)/WelcomeUsername/js/app.js
@@ -1,5 +1,5 @@
 function showGreetingMessage() {
    var name = document.getElementById('uname').value;
    name = $.trim(name);
-   document.write("Welcome" + name);
+   document.write("Welcome " + name + " !");
 }
