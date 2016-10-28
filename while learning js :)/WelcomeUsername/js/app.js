ee014d3014ca24394cf3435814cdad613cbb51ad
Hasmik Naslyan <hasmik_naslyan@mail.ru>
2016-10-28 12:18:03 +0400

    Corrected the functionality of the fields validation.

--- while learning js :)/WelcomeUsername/js/app.js
+++ while learning js :)/WelcomeUsername/js/app.js
@@ -1,23 +1,28 @@
 function showGreetingMessage() {
-   if(validateForm()){
-   var username = document.getElementById('uname').value;
-      username = $.trim(username);
-   $("label").hide();
-   $("input").hide();
-   $("button").hide();
-   var paragraph = document.createElement("p");
-   var node = document.createTextNode("Welcome " + username);
-      paragraph.appendChild(node);
-   var element = document.getElementById("container");
-   element.appendChild(paragraph);}
-}
 
-function validateForm() {
-   var username = document.forms["form"]["uname"].value;
-   var password = document.forms["form"]["psw"].value;
-   if (username == null || username == "" || password==null || password=="") {
-      alert("The credentials of the user must be filled out");
-      return false;
-   }
-   return true;
+    $("#myform").validate({
+        rules: {
+            username: "required",
+            password: "required"
+        },
+        messages: {
+            uname: "Please specify username",
+            psw: "Please specify your password"
+}
+});
+
+    if ($("#myform").valid()) {
+
+    var username = document.getElementById('uname').value;
+    username = $.trim(username);
+    $("label").hide();
+    $("input").hide();
+    $("button").hide();
+    var paragraph = document.createElement("p");
+    var node = document.createTextNode("Welcome " + username);
+    paragraph.appendChild(node);
+    var element = document.getElementById("container");
+    element.appendChild(paragraph);
+
+}
 }
