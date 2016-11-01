521c1b7cadbefe7c082266a96712743a6d34a220
Hasmik Naslyan <hasmik_naslyan@mail.ru>
2016-11-01 13:04:40 +0400

    Updated WordList task.

--- while learning js :)/WordsList/js/app.js
+++ while learning js :)/WordsList/js/app.js
@@ -1,27 +1,38 @@
 var words = [];
 var uniqueWords = [];
-i=0;
+listItemId=0;
+
 function addWord() {
-    var newWord = document.getElementById('newWord').value;
+    var newWord = $('#newWord').val();
     words.push(newWord.toLowerCase());
-    var ul = document.getElementById("list");
-    var li = document.createElement("li");
+    var ul = document.getElementById('list');
+    var li = document.createElement("LI");
     li.appendChild(document.createTextNode(newWord));
+    li.setAttribute('id',listItemId);
+    var image = document.createElement("IMG");
+    image.setAttribute("src", "../resources/img/delete.jpg");
+    image.setAttribute("alt", "Delete Icon");
+    image.setAttribute("padding-top","1px");
+    var removingButton = document.createElement("BUTTON");
+    removingButton.setAttribute("id", listItemId);
+    removingButton.setAttribute("onclick", 'removeWord(event)');
+    listItemId++;
+    removingButton.appendChild(image);
+    li.appendChild(removingButton);
     ul.appendChild(li);
     cleanInputBox();
     updateUniqueWordsList(newWord);
+}
+
+function removeWord(event){
+   // alert("***" + event);
 }
 
 function cleanInputBox(){
     $('#newWord').val("");
 }
 
-function removeWord() {
-
-}
-
 function updateUniqueWordsList(word) {
-
     $.each(words, function(i, word){
         if($.inArray(word, uniqueWords) === -1) uniqueWords.push(word);
     })
