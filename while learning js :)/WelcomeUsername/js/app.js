fbd4259f86c7d241b36e2b1f00dfc1919c41bd0f
Hasmik Naslyan <hasmik_naslyan@mail.ru>
2016-11-01 12:36:28 +0400

    Made improvements on WelcomeUsername.

--- while learning js :)/WelcomeUsername/js/app.js
+++ while learning js :)/WelcomeUsername/js/app.js
@@ -1,29 +1,23 @@
-function showGreetingMessage() {
-
-    $("#myform").validate({
+$( document ).ready(function() {
+    $("#myForm").validate({
         rules: {
-            username: "required",
-            password: "required"
+            username: 'required',
+            password: 'required'
         },
         messages: {
-            username: "Please specify username",
-            password: "Please specify your password"
-
+            username: 'Please specify username',
+            password: 'Please specify your password'
         },
         errorElement: 'div'
+    });
 });
 
-    if ($("#myform").valid()) {
-
-    var username = document.getElementById('uname').value;
-    username = $.trim(username);
-    $("label").hide();
-    $("input").hide();
-    $("button").hide();
-    var paragraph = document.createElement("p");
-    var node = document.createTextNode("Welcome " + username);
-    paragraph.appendChild(node);
-    var element = document.getElementById("container");
-    element.appendChild(paragraph);
-}
+function showGreetingMessage() {
+    if ($("#myForm").valid()) {
+    var username = $.trim($("#uname").val());
+    $(".hidden-element").hide();
+        var greetingText = $("#greetingText");
+        $(greetingText).html( 'Welcome ' + username);
+        $(greetingText).css('display', 'block');
+    }
 }
