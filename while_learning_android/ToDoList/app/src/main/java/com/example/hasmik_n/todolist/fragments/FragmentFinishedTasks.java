cfe31f6db331263a3ad32952f291c2c308182790
Hasmik Naslyan <hasmik_n@instigatemobile.com>
2017-09-20 17:45:25 +0400

    Bug fix regarding searching.

--- while_learning_android/ToDoList/app/src/main/java/com/example/hasmik_n/todolist/fragments/FragmentFinishedTasks.java
+++ while_learning_android/ToDoList/app/src/main/java/com/example/hasmik_n/todolist/fragments/FragmentFinishedTasks.java
@@ -2,9 +2,14 @@ package com.example.hasmik_n.todolist.fragments;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -22,10 +27,12 @@ import java.util.ArrayList;
 public class FragmentFinishedTasks extends Fragment {
 
     private ArrayList<Task> tasks;
+    private TasksRecyclerViewAdapter mAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         tasks = getArguments().getParcelableArrayList("finishedTasksList");
+        setHasOptionsMenu(true);
         return inflater.inflate(R.layout.fragment_finished_tasks, container, false);
     }
 
@@ -35,7 +42,33 @@ public class FragmentFinishedTasks extends Fragment {
         RecyclerView tasksList = getView().findViewById(R.id.finished_tasks_recycler_view);
         tasksList.setHasFixedSize(true);
         tasksList.setLayoutManager(new LinearLayoutManager(getContext()));
-        RecyclerView.Adapter mAdapter = new TasksRecyclerViewAdapter(tasks);
+        mAdapter = new TasksRecyclerViewAdapter(tasks);
         tasksList.setAdapter(mAdapter);
+    }
+
+    // TODO some kind of base class should be used for all tasks and finished tasks fragments
+
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        MenuItem search = menu.findItem(R.id.search);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(search);
+        search(searchView);
+        super.onCreateOptionsMenu(menu,inflater);
+    }
+
+    private void search(SearchView searchView) {
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                mAdapter.getFilter().filter(newText);
+                return true;
+            }
+        });
     }
 }
